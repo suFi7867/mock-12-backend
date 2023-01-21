f1826f6fa11dbbf6f28734bccd496004366ec27f
suFi7867 <noorani786.ss@gmail.com>
2023-01-21 12:17:01 +0530

    SCHEMA ROUTE

--- src/routes/user/user.model.js
+++ src/routes/user/user.model.js
@@ -2,6 +2,7 @@ const {Schema,model} = require('mongoose')
 
 const UserSchema = new Schema({
 
+    
 username : { type:String, required:true },
 email:{type:String, unique:true},
 password:{ type:String, required:true },
